a7ddbba64512b15d602d2ea8c67b0ed67537b700
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2026-01-06 19:59:29 +0000

    Correct JSX syntax for Upwork availability paragraph
    Fix JSX syntax error in the Contacts component.

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -19,7 +19,7 @@ const NextSteps = () => {
         <SiTailwindcss className={css.icon} title="Tailwind CSS" />
         <FaUpwork className={css.icon} title="Available on Upwork" />
       </div> */}
-<p>
+<p className={css.availableUpwork)>
 © {new Date().getFullYear()} All rights reserved.
   <a href="https://www.upwork.com/freelancers/~01ab7fe2a9e7d90639" className={css.footerNote}>
          Available on Upwork.
